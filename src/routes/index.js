b5a5721967abd0f7ba81e9b1ce5a2086d2c55ece
BrunoSouza <desouza88@hotmail.com>
2023-05-11 21:46:38 -0300

    Requisito 04

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { loginController } = require('../controllers/loginController');
+const { createUser } = require('../controllers/userController');
+const {
+  displayNameValidator,
+  emailValidator,
+  passwordValidator,
+} = require('../middlewares/Validations');
 
 const apiRoutes = express.Router();
 
 apiRoutes.post('/login', loginController);
+apiRoutes.post('/user', emailValidator, displayNameValidator, passwordValidator, createUser);
 
 module.exports = apiRoutes;
